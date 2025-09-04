5eaa262d7af06c30e11589b736631c8bb8dc729b
Funmilola Akanbi <funmilolaakanbi44@gmail.com>
2025-09-05 02:51:53 +0400

    Update nav links to use lowercase destination.html

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         main: "./index.html",
         crew: "./crew.html",
         technology: "./technology.html",
-        destination: "./destination.html",
+        destination: "./Destination.html",
       },
     },
   },
