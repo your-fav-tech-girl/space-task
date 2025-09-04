37a174ba3c0bce7ce6082e185711efa511593b86
Funmilola Akanbi <funmilolaakanbi44@gmail.com>
2025-09-04 17:29:56 +0400

    updated vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(), // ✅ Tailwind v4 plugin for Vite
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        crew: "crew.html",
+        technology: "technology.html",
+        destination: "destination.html",
+      },
+    },
+  },
 });
